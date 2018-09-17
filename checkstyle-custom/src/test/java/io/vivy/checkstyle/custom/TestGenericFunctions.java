02381a6ce8c92f1ff77bf48ddfb63c1ed067ccd9
oyewaleoyelami <@Busy509>
2018-09-17 11:29:51 +0200

    code cleanup

--- checkstyle-custom/src/test/java/io/vivy/checkstyle/custom/TestGenericFunctions.java
+++ checkstyle-custom/src/test/java/io/vivy/checkstyle/custom/TestGenericFunctions.java
@@ -8,7 +8,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestGenericFunctions{
+public class TestGenericFunctions {
 
     protected Checker prepareCheckStyleChecker(Class testClass) throws CheckstyleException {
         Checker checker = new Checker();
