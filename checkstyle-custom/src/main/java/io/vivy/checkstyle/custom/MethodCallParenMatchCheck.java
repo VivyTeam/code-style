f02c44541ba9cd12bb03bad38a1879249bb7adfd
oyewaleoyelami <@Busy509>
2018-10-05 15:03:21 +0200

    added testcase for methodparenmatch check for chained method calls with return

--- checkstyle-custom/src/main/java/io/vivy/checkstyle/custom/MethodCallParenMatchCheck.java
+++ checkstyle-custom/src/main/java/io/vivy/checkstyle/custom/MethodCallParenMatchCheck.java
@@ -60,8 +60,7 @@ public class MethodCallParenMatchCheck extends AbstractCheck {
 
                 if (lineText.contains("return")) {
                     leftParenColumnNo = lineText.indexOf("return");
-                    if ((rightParenText.contains("))")) || (rightParenText.contains(")))")) || (rightParenText.contains("})"))
-                            || (rightParenText.contains(");"))) {
+                    if ((rightParenText.contains("))")) || (rightParenText.contains(");")) || (rightParenText.contains("})"))) {
                         if (leftParenText.trim().charAt(0) == '.') {
                             return; //ignore this because of the conflict with lambda indent check
                         }
@@ -72,8 +71,7 @@ public class MethodCallParenMatchCheck extends AbstractCheck {
                         if (leftParenColumnNo != (rightParenToken.getColumnNo() + rightCurlyParen)) {
                             log(leftParenToken, RETURN_MSG_KEY);
                         }
-                } else if ((rightParenText.contains("))")) || (rightParenText.contains(")))")) || (rightParenText.contains("})"))
-                        || (rightParenText.contains(");"))) {
+                } else if ((rightParenText.contains("))")) || (rightParenText.contains(");")) || (rightParenText.contains("})"))) {
                     if (leftParenText.trim().charAt(0) == '.') {
                         return; //ignore this because of the conflict with lambda indent check
                     }
